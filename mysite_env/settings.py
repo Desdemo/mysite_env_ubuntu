15aa8a56585b658b61aae2b57cefdf9fb6976cbd
desdemo <1184788560@qq.com>
2018-09-10 09:07:31 +0800

    数据库迁移到mysql

--- mysite_env/settings.py
+++ mysite_env/settings.py
@@ -81,14 +81,24 @@ WSGI_APPLICATION = 'mysite_env.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysite_env_db',
+        'USER': 'desdemo',
+        'PASSWORD': 'desdemo',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
