489db00a55fb80c27a3f51fc0603720fa9e2d3eb
desdemo <1184788560@qq.com>
2018-09-17 16:41:36 +0800

    修复登录、注册URL叠加问题

--- user/models.py
+++ user/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     nickname = models.CharField(max_length=20, verbose_name="昵称")
 
     def __str__(self):
         return '<Profile: %s for %s>' %(self.nickname, self.user.username)
+
 
 def get_nickname(self):
     if Profile.objects.filter(user=self).exists():
@@ -15,6 +17,7 @@ def get_nickname(self):
     else:
         return ''
 
+
 def get_nickname_username(self):
     if Profile.objects.filter(user=self).exists():
         profile = Profile.objects.get(user=self)
@@ -22,8 +25,10 @@ def get_nickname_username(self):
     else:
         return self.username
 
+
 def has_nickname(self):
     return Profile.objects.filter(user=self).exists()
+
 
 User.get_nickname = get_nickname
 User.has_nickname = has_nickname
